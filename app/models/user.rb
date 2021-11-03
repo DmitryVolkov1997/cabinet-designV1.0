b848f9ae3ae366a440fe58e6170f3ecacff37cb4
Dmitry Volkov <dmitrij_volkov_2014@mail.ru>
2021-11-03 19:05:31 +0600

    исправил удалений записей и сменил надпись на Абитуриент уже существует

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 class User < ApplicationRecord
+  validates :first_name, length: { minimum: 1, maximum: 50 }, allow_blank: false, presence: true
 
-    validates :first_name, length: {minimum: 1, maximum: 50}, allow_blank: false, presence: true
+  validates :last_name, length: { minimum: 1, maximum: 50 }, allow_blank: false, presence: true
 
-    validates :last_name, length: {minimum: 1, maximum: 50}, allow_blank: false, presence: true
+  validates :phone, format: { with: /\A\d+\z/, message: 'разрешен ввод только цифр' }
 
-    validates :phone, format: { with: /\A\d+\z/, message: "разрешен ввод только цифр" }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'введите корректный email-адрес' }
 
-    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "введите корректный email-адрес" }
+  validate :email_uniqueness
 
-    validates :first_name, uniqueness: { scope: [ :last_name, :email ] }
+  private
 
-    has_one :request, dependent: :destroy, :inverse_of => :user
+  def email_uniqueness
+    errors.add(:base, 'Абитуриент уже существует') if User.where('LOWER(email) = ?', email.downcase).exists?
+  end
 
+  validates :first_name, uniqueness: { scope: %i[last_name email] }
+ 
+  has_many :requests, dependent: :destroy
+
+  has_one :request, inverse_of: :user
 end
-
-
-
